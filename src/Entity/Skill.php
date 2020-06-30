c73e9101151dea74e64ca35ba4487d36a52f7d44
Chheany Mok <cmok4290@gmail.com>
2020-06-30 10:43:42 -0400

    Fix annotation import

--- src/Entity/Skill.php
+++ src/Entity/Skill.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\SkillRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
